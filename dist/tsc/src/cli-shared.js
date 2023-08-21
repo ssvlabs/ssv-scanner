5315860d8608aeedc0362f7471c58f7e86c460b1
Vadim <vadim.inc@gmail.com>
2023-08-21 16:44:14 +0300

    remove unused code

--- dist/tsc/src/cli-shared.js
+++ dist/tsc/src/cli-shared.js
@@ -48,56 +48,6 @@ function main() {
                 console.error('Command not found');
                 process.exit(1);
         }
-        /*
-        const parser = new ArgumentParser();
-      
-        parser.add_argument('-n', '--node-url', {
-          help: `The ETH1 node url.`,
-          required: true,
-          dest: 'nodeUrl'
-        });
-        parser.add_argument('-ca', '--ssv-contract-address', {
-          help:
-            'The SSV Contract address, used to find the latest cluster data snapshot. ' +
-            'Refer to https://docs.ssv.network/developers/smart-contracts',
-          required: true,
-          dest: 'contractAddress'
-        });
-        parser.add_argument('-oa', '--owner-address', {
-          help: "The owner address regarding the cluster that you want to query",
-          required: true,
-          dest: 'ownerAddress'
-        });
-        parser.add_argument('-oids', '--operator-ids', {
-          help: `Comma-separated list of operators IDs regarding the cluster that you want to query`,
-          required: true,
-          dest: 'operatorIds'
-        });
-        */
-        /*
-        try {
-          let params = parser.parse_args();
-          const operatorIds = params.operatorIds.split(',')
-            .map((value: any) => {
-              if (Number.isNaN(+value)) throw new Error('Operator Id should be the number');
-              return +value;
-            })
-            .sort((a: number, b: number) => a - b);
-      
-          const command = new SSVScannerCommand(params);
-          const result = await command.cluster(operatorIds, true);
-          console.table(result.payload);
-          console.log('Cluster snapshot:');
-          console.table(result.cluster);
-          console.log(JSON.stringify({
-            'block': result.payload.Block,
-            'cluster snapshot': result.cluster,
-            'cluster': Object.values(result.cluster)
-          }, null, '  '));
-        } catch (e: any) {
-          console.error('\x1b[31m', e.message);
-        }
-        */
     });
 }
 exports.default = main;
