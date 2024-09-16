ef6a368bc417808a2f2375e097a446c41778ab4d
Ilya <ilya@blox.io>
2024-09-16 15:12:39 +0300

    added build files

--- dist/tsc/src/lib/NonceScanner/NonceScanner.js
+++ dist/tsc/src/lib/NonceScanner/NonceScanner.js
@@ -2,81 +2,68 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.NonceScanner = void 0;
 const tslib_1 = require("tslib");
+const ethers_1 = require("ethers");
 const cli_progress_1 = tslib_1.__importDefault(require("cli-progress"));
 const contract_provider_1 = require("../contract.provider");
 const BaseScanner_1 = require("../BaseScanner");
 class NonceScanner extends BaseScanner_1.BaseScanner {
-    constructor() {
-        super(...arguments);
-        this.eventsList = [
-            'ValidatorAdded',
-        ];
-    }
-    async run(cli) {
-        if (cli) {
+    async run(isCli) {
+        if (isCli) {
             console.log('\nScanning blockchain...');
             this.progressBar = new cli_progress_1.default.SingleBar({}, cli_progress_1.default.Presets.shades_classic);
         }
         try {
-            const data = await this._getLatestNonce(cli);
-            cli && this.progressBar.stop();
+            const data = await this._getValidatorAddedEventCount(isCli);
+            isCli && this.progressBar.stop();
             return data;
         }
         catch (e) {
-            cli && this.progressBar.stop();
+            isCli && this.progressBar.stop();
             throw new Error(e);
         }
     }
-    async _getLatestNonce(cli) {
-        const contractProvider = new contract_provider_1.ContractProvider(this.params.network, this.params.nodeUrl);
+    async _getValidatorAddedEventCount(isCli) {
+        const { contractAddress, abi, genesisBlock } = (0, contract_provider_1.getContractSettings)(this.params.network);
+        const provider = new ethers_1.ethers.JsonRpcProvider(this.params.nodeUrl);
+        const contract = new ethers_1.ethers.Contract(contractAddress, abi, provider);
         let latestBlockNumber;
         try {
-            latestBlockNumber = await contractProvider.web3.eth.getBlockNumber();
+            latestBlockNumber = await provider.getBlockNumber();
         }
         catch (err) {
             throw new Error('Could not access the provided node endpoint.');
         }
         try {
-            await contractProvider.contractCore.methods.owner().call();
+            await contract.owner();
         }
         catch (err) {
             throw new Error('Could not find any cluster snapshot from the provided contract address.');
         }
-        let step = this.MONTH;
-        let latestNonce = 0;
-        const genesisBlock = contractProvider.genesisBlock;
-        const ownerTopic = contractProvider.web3.eth.abi.encodeParameter('address', this.params.ownerAddress);
-        const filters = {
-            fromBlock: genesisBlock,
-            toBlock: Number(latestBlockNumber),
-            topics: [null, ownerTopic],
-        };
-        cli && this.progressBar.start(Number(latestBlockNumber), 0);
-        do {
-            let result;
+        let totalEventCount = 0;
+        let blockStep = this.MONTH;
+        isCli && this.progressBar.start(Number(latestBlockNumber), 0);
+        const filter = contract.filters.ValidatorAdded(this.params.ownerAddress);
+        for (let startBlock = genesisBlock; startBlock <= latestBlockNumber; startBlock += blockStep) {
             try {
-                result =
-                    (await contractProvider.contractCore.getPastEvents('allEvents', filters))
-                        .filter((item) => this.eventsList.includes(item.event));
-                latestNonce += result.length;
-                filters.fromBlock = filters.toBlock + 1;
+                const endBlock = Math.min(startBlock + blockStep - 1, latestBlockNumber);
+                const logs = await contract.queryFilter(filter, startBlock, endBlock);
+                totalEventCount += logs.length;
+                isCli && this.progressBar.update(endBlock);
             }
-            catch (e) {
-                if (step === this.MONTH) {
-                    step = this.WEEK;
+            catch (error) {
+                if (blockStep === this.MONTH) {
+                    blockStep = this.WEEK;
                 }
-                else if (step === this.WEEK) {
-                    step = this.DAY;
+                else if (blockStep === this.WEEK) {
+                    blockStep = this.DAY;
                 }
                 else {
-                    throw new Error(e);
+                    throw new Error(error);
                 }
             }
-            filters.toBlock = Math.min(filters.fromBlock + step, Number(latestBlockNumber));
-            cli && this.progressBar.update(filters.toBlock);
-        } while (filters.toBlock - filters.fromBlock > 0);
-        cli && this.progressBar.update(Number(latestBlockNumber), Number(latestBlockNumber));
-        return latestNonce;
+        }
+        isCli && this.progressBar.update(latestBlockNumber, latestBlockNumber);
+        return totalEventCount;
     }
 }
 exports.NonceScanner = NonceScanner;
