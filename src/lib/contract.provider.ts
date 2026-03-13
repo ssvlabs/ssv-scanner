369ecd88616824f7784acac62332098026a729ce
AlexM-ssv-labs <alexmi@ssvlabs.io>
2026-03-13 18:34:38 +0200

    fix: removing changes from contract.provider

--- src/lib/contract.provider.ts
+++ src/lib/contract.provider.ts
@@ -9,19 +9,14 @@ export const ContractVersion = {
 const getContractSettings = (networkAndEnv: string) => {
   const [contractEnv, contractNetwork] = ContractVersion[networkAndEnv.toUpperCase() as keyof typeof ContractVersion].split(':');
 
-  const configFileName = `${contractEnv}.${contractNetwork}.abi.json`;
-
   let jsonCoreData;
   try {
     // eslint-disable-next-line @typescript-eslint/no-var-requires
-    jsonCoreData = require(`../shared/abi/${configFileName}`);
+    jsonCoreData = require(`../shared/abi/${contractEnv}.${contractNetwork}.abi.json`);
   } catch (err) {
-    console.error(`Failed to load JSON data from ${configFileName}`, err);
+    console.error(`Failed to load JSON data from ${contractEnv}.${contractNetwork}.abi.json`, err);
     throw err;
   }
-
-  // One-time log per invocation so we know which config was used indeed.
-  console.log(`\nContract config file in use: ${configFileName}\n`);
 
   // Check if required properties exist in jsonData
   if (
